8cc7e41d55383c1c4fa57ea718ff77579ff8eb23
Afonso Fernandes <81565090+DivachiLeonerd@users.noreply.github.com>
2023-12-12 10:45:17 +0000

    Add project files.

---
+++ Say Hello WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Say_Hello_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand;
+        public MainWindow()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+        private byte[] GetRandomBytes(int n)
+        {
+            // Fill an array of bytes of length "n" with random numbers.
+            byte[] randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHello_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            // Create a solid color brush using the three random numbers.
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            sayHello_TextBox.Text = "Hello World!";
+            // Set both the text color and the text box border to the random color.
+            sayHello_TextBox.BorderBrush = randomColorBrush;
+            sayHello_TextBox.Foreground = randomColorBrush;
+
+        }
+    }
+}
